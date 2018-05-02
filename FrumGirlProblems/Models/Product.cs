0c289f7c1a209f07f2320582ff83e487ce3b29e0
Adam McKenna <adam.mckenna87@gmail.com>
2018-05-02 10:28:06 -0500

    Added product controller and product view

--- FrumGirlProblems/Models/Product.cs
+++ FrumGirlProblems/Models/Product.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace FrumGirlProblems.Models
 {
-    public class t_shirt {
+    public class Product {
 
             public int ID { get; set; }
             public string Name { get; set; }
