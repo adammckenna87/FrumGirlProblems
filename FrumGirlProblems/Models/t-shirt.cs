38266b0954a029c88558abe2554ceef30ebde7e6
Adam McKenna <adam.mckenna87@gmail.com>
2018-05-02 10:17:45 -0500

    new model

---
+++ FrumGirlProblems/Models/t-shirt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrumGirlProblems.Models
+{
+    public class t_shirt {
+
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal? Price { get; set; }
+            public string Image { get; set; }
+
+    }
+}
